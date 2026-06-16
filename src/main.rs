16e5465b49e0922760031287f6960ede2ae8a0a3
Mike <mikeniehaus85@gmail.com>
2026-06-15 22:13:43 -0400

    v0.5.4.214: play mine explosion sound on black hole collapse

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod game;
 mod net;
 mod updater;
 mod audio;
-const VERSION: &str = "0.5.4.213";
+const VERSION: &str = "0.5.4.214";
 
 use std::time::{Duration, Instant};
 use world::{WorldPos, Heightmap, Terrain, WORLD_W};
